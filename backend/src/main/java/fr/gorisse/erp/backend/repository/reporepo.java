def8326a6375f32fa571627afa652f9cba556839
gt102769 <thomas.gorisse@etu.unice.fr>
2023-11-02 13:43:53 +0100

    fixme

---
+++ backend/src/main/java/fr/gorisse/erp/backend/repository/reporepo.java
@@ -0,0 +1,7 @@
+package fr.gorisse.erp.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+@NoRepositoryBean
+public interface reporepo<T> extends JpaRepository<T, Integer> {
+}
--- backend/src/main/java/fr/gorisse/erp/backend/services/interfaces/UserCheckingServiceInterface.java
+++ backend/src/main/java/fr/gorisse/erp/backend/services/interfaces/UserCheckingServiceInterface.java
@@ -5,10 +5,6 @@ import fr.gorisse.erp.backend.entity.User;
 import java.util.ArrayList;
 
 public interface UserCheckingServiceInterface {
-    User create (User user);
-    ArrayList<User> getUsers();
-    User getUserById(int user_id);
     User getUserByLogin(String login);
-    int getNumberOfUsers();
-    void deleteUser(User user);
+
 }
