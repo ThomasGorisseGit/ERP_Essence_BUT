10b9b443e91543f7b74c42101e14d7336b2ca94d
Thomas <thomas.gorisse07@gmail.com>
2023-11-07 17:16:55 +0100

    [backend] Provider correction

--- backend/src/main/java/fr/gorisse/erp/backend/services/ProviderService.java
+++ backend/src/main/java/fr/gorisse/erp/backend/services/ProviderService.java
@@ -4,6 +4,7 @@ import fr.gorisse.erp.backend.entity.Product;
 import fr.gorisse.erp.backend.entity.Provider;
 import fr.gorisse.erp.backend.entity.User;
 import fr.gorisse.erp.backend.entity.valueObject.Siret;
+import fr.gorisse.erp.backend.repository.ProductRepository;
 import fr.gorisse.erp.backend.repository.ProviderRepository;
 import fr.gorisse.erp.backend.services.interfaces.ServiceMethodsInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +16,29 @@ import java.util.List;
 @Service
 public class ProviderService extends ServiceMethods<Provider>{
     private ProviderRepository providerRepository;
-
+    private ProductRepository productRepository;
     @Autowired
-    protected void setRepository(ProviderRepository providerRepository) {
+    protected void setRepository(ProviderRepository providerRepository, ProductRepository productRepository) {
         this.repository = providerRepository;
         this.providerRepository = providerRepository;
+        this.productRepository = productRepository;
     }
     @Override
     public Provider create(Provider provider){
-
-        return super.create(provider);
+        if(!provider.getProductList().isEmpty()){
+            for(Product p : provider.getProductList()){
+                p.setProvider(provider);
+            }
+        }
+        provider = super.create(provider);
+        this.productRepository.saveAll(provider.getProductList());
+        return provider;
     }
 
     public List<Product> getProductList(Provider provider){
-        //TODO : Display Product list
-        return null;
+        return this.productRepository.findByProviderId(provider.getId());
+    }
+    public List<Product> getProductListByID(int id){
+        return this.productRepository.findByProviderId(id);
     }
 }
