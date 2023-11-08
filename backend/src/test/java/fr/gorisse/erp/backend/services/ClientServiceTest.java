cf33005c8079d2d01bd941b069aedd0d2180f429
gt102769 <thomas.gorisse@etu.unice.fr>
2023-11-08 09:11:51 +0100

    [Backend] Client basic Insertion Test

---
+++ backend/src/test/java/fr/gorisse/erp/backend/services/ClientServiceTest.java
@@ -0,0 +1,56 @@
+package fr.gorisse.erp.backend.services;
+
+import fr.gorisse.erp.backend.entity.Client;
+import fr.gorisse.erp.backend.exceptions.DataNotFounded;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ClientServiceTest {
+
+    @Autowired
+    private ClientService clientService;
+    @Autowired
+    private SubscriptionService subscriptionService;
+    @Autowired
+    private ClientOrderService clientOrderService;
+
+    private int numberOfClients;
+    private Client client;
+
+    @BeforeEach
+    public void init(){
+        this.client = new Client();
+        this.numberOfClients = this.clientService.getNumberOfEntity();
+    }
+    @Test
+    public void testBasicClientInsertion(){
+        int tempNumberOfUsers = this.clientService.getNumberOfEntity();
+
+        assertEquals(tempNumberOfUsers,this.numberOfClients);
+        assertThrows(DataNotFounded.class,()->this.clientService.getEntityById(this.client.getId()));
+        Client insertedClient = this.clientService.create(this.client);
+        tempNumberOfUsers = this.clientService.getNumberOfEntity();
+
+        assertNotEquals(tempNumberOfUsers,this.numberOfClients);
+        assertDoesNotThrow(()->this.clientService.getEntityById(this.client.getId()));
+        assertEquals(insertedClient.getId(),this.client.getId());
+    }
+    @Test
+    public void testSubscription(){
+        this.client = this.clientService.create(this.client);
+
+
+
+
+    }
+    @AfterEach
+    public void destroy(){
+        this.clientService.delete(client);
+    }
+}
