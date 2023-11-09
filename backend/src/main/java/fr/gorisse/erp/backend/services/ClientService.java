2f44230f134b8e2943a5b704ca68c0b999024fb9
Thomas <thomas.gorisse07@gmail.com>
2023-11-09 10:55:39 +0100

    [backend] Clean default values

--- backend/src/main/java/fr/gorisse/erp/backend/services/ClientService.java
+++ backend/src/main/java/fr/gorisse/erp/backend/services/ClientService.java
@@ -1,46 +1,47 @@
 package fr.gorisse.erp.backend.services;
 
 import fr.gorisse.erp.backend.entity.Client;
-import fr.gorisse.erp.backend.entity.ClientOrder;
 import fr.gorisse.erp.backend.entity.Subscription;
 import fr.gorisse.erp.backend.exceptions.DataNotFounded;
 import fr.gorisse.erp.backend.repository.ClientRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ClientService extends ServiceMethods<Client>{
+public class ClientService extends ServiceMethods<Client> {
     private SubscriptionService subscriptionService;
+
     @Autowired
     protected void setRepository(ClientRepository clientRepository, SubscriptionService subscriptionService) {
         super.repository = clientRepository;
         this.subscriptionService = subscriptionService;
     }
+
     @Override
-    public Client create(Client client){
-
-
-        setSubscription(client);
-
-
-
+    public Client create(Client client) {
+        defaultValues(client);
         return super.create(client);
     }
 
-    private void setSubscription(Client client){
+    private void setSubscription(@NotNull Client client) {
         Subscription sub;
-        if(client.getSubscription()==null){
-             sub = subscriptionService.getFreePlan();
-        }
-        else{
-            try{
+        if (client.getSubscription() == null) {
+            sub = subscriptionService.getFreePlan();
+        } else {
+            try {
                 sub = subscriptionService.getEntityById(client.getSubscription().getId());
-            } catch (DataNotFounded e){
+            } catch (DataNotFounded e) {
                 sub = subscriptionService.getFreePlan();
             }
 
         }
         client.setSubscription(sub);
+    }
+
+    private void defaultValues(Client client) {
+        setSubscription(client);
+        if (client.getFirstName() == null) client.setFirstName("");
+        if (client.getLastName() == null) client.setLastName("");
     }
 }
