4e2e83c611a325a4b8429d69eebc284cd36c4175
Thomas <thomas.gorisse07@gmail.com>
2023-11-17 08:54:55 +0100

    [frontend] Product

--- backend/src/main/java/fr/gorisse/erp/backend/repository/ProductRepository.java
+++ backend/src/main/java/fr/gorisse/erp/backend/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package fr.gorisse.erp.backend.repository;
 
 import fr.gorisse.erp.backend.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends DefaultRepository<Product> {
     List<Product> findByProviderId(int provider_id);
+    List<Product> findByProviderIsNull();
+    List<Product> findByProviderIsNotNull();
+
 }
