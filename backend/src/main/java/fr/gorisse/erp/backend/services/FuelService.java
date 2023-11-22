2593e7037071d48241581500d2add6faa1ffbd20
Thomas <thomas.gorisse07@gmail.com>
2023-11-22 20:22:20 +0100

    [frontend] Decrease fuel qte automatically

--- backend/src/main/java/fr/gorisse/erp/backend/services/FuelService.java
+++ backend/src/main/java/fr/gorisse/erp/backend/services/FuelService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class FuelService extends ServiceMethods<Fuel>{
@@ -17,6 +18,26 @@ public class FuelService extends ServiceMethods<Fuel>{
 
     public void createAll(List<Fuel> fuelList) {
         super.repository.saveAll(fuelList);
+    }
+    @Override
+    public List<Fuel> getAll(){
+        return this.lowStock();
+    }
+
+    /**
+     * Automatically decrease the quantity of fuel
+     * @return List<Fuel> the list of fuel with a potentially decreased quantity
+     */
+     private List<Fuel> lowStock(){
+        List<Fuel> fuelLIst = super.getAll();
+        for(Fuel f : fuelLIst){
+            Random r = new Random();
+            int rdm = r.nextInt(100);
+            if(rdm <30 && f.getQuantity()>rdm){
+                f.setQuantity(f.getQuantity()-rdm);
+            }
+        }
+        return super.repository.saveAll(fuelLIst);
     }
     public Fuel addQte(int quantity, int id){
         Fuel f = super.getEntityById(id);
