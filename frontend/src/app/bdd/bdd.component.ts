7848360e89b4b1d471774699355a7da079dda78b
Thomas <thomas.gorisse07@gmail.com>
2023-12-12 13:11:04 +0100

    Table example

--- frontend/src/app/bdd/bdd.component.ts
+++ frontend/src/app/bdd/bdd.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from 'src/app/_services/product.service';
 import { Component } from '@angular/core';
+import { Product } from '../_interfaces/product';
 
 @Component({
   selector: 'app-bdd',
@@ -7,11 +9,28 @@ import { Component } from '@angular/core';
 })
 export class BddComponent {
   private selectedPage:string = "Stocks";
-
+  listProductWithStocks: Product[] = []
   getSelectedPage() :string{
     return this.selectedPage;
   }
   setSelectedPage(page:string){
     this.selectedPage = page;
+  }
+
+
+  constructor(private productService:ProductService) {
+    this.fetchStocks();
+  }
+
+  fetchStocks(){
+    if(this.productService.listProductWithStocks === null ){
+      this.productService.ProductContainsAll().subscribe({
+        next:(data)=>{
+          this.listProductWithStocks = data;
+        }
+      })
+    }else{
+      this.listProductWithStocks = this.productService.listProductWithStocks as Product[];
+    }
   }
 }
--- frontend/src/app/bdd/content/content.component.ts
+++ frontend/src/app/bdd/content/content.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, Provider } from '@angular/core';
+import { Client } from 'src/app/_interfaces/client';
+import { Fuel } from 'src/app/_interfaces/fuel';
+import { Product } from 'src/app/_interfaces/product';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  @Input()
+   productList :Product[] = [];
+  @Input()
+  fuelList :Fuel[] = [];
+  @Input()
+  providerList :Provider[] = [];
+  @Input()
+  clientList :Client[] = [];
 
 }
